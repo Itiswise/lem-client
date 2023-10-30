b3dd668f0b9ee9ad69f94cb7d3b5df5dde099df1
Jakub Topoliński <129954536+jtopolinski@users.noreply.github.com>
2023-10-30 16:42:01 +0100

    Update ROOT_URL variable

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const ROOT_URL =
   process.env.NODE_ENV === "production"
-    ? "https://riverdi-lem.herokuapp.com"
+    ? "https://api-riverdi.itiswise.com"
     : "http://localhost:3090";
 
 export const headers = {
