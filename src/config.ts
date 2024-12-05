f1e3080905c8bb810b5797f264cb4c269652e323
SebastianBurzych <s.burzych@itiswise.com>
2024-12-05 11:49:10 +0100

    refactor config.ts and package.json for ROOT_URL

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
 export const ROOT_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://api-riverdi.itiswise.com"
+    process.env.REACT_APP_ENV === "production"
+        ? "https://api-riverdi.itiswise.com" :
+        process.env.REACT_APP_ENV === "staging" ?
+            "https://staging-api-riverdi.itiswise.com"
     : "http://localhost:3090";
 
 export const headers = {
